94cf83b6a7acc3e346e1945cf139ffdcb21cf42d
gongwho <hklee4000@gmail.com>
2020-11-08 23:36:28 +0900

    some issues solved

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 var global = this;
 var state = null;
+
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 	if (changeInfo.status == 'complete') {
-		console.log(Date.now());
+		let timeNow = new Date();
+		console.log(timeNow.getHours() + ":" + timeNow.getMinutes() + ":" + timeNow.getSeconds() + ":" + timeNow.getMilliseconds());
 		//chrome.tabs.executeScript({code:"console.log('hello! from the background page');"}) ;
 		chrome.runtime.getPackageDirectoryEntry(function(root) {
 			root.getFile("state.txt", {}, function(fileEntry) {
@@ -16,7 +18,7 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 								console.log("response: " + response.txt);
 							}
 							else {
-								console.log("error");
+								console.log("not a youtube page");
 							}
 						});
 					};
@@ -25,4 +27,32 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 			});
 		});
 	}
-})
+});
+
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	console.log("tab activated");
+    let timeNow = new Date();
+	console.log(timeNow.getHours() + ":" + timeNow.getMinutes() + ":" + timeNow.getSeconds() + ":" + timeNow.getMilliseconds());
+	//chrome.tabs.executeScript({code:"console.log('hello! from the background page');"}) ;
+	chrome.runtime.getPackageDirectoryEntry(function(root) {
+		root.getFile("state.txt", {}, function(fileEntry) {
+			fileEntry.file(function(file) {
+				var reader = new FileReader();
+				reader.onloadend = function(e) {
+					console.log("loaded!!");
+					global.state = this.result;
+					chrome.tabs.sendMessage(activeInfo.tabId, {state: global.state}, function(response) {
+						if (!window.chrome.runtime.lastError) {
+							console.log("response: " + response.txt);
+						}
+						else {
+							console.log("not a youtube page");
+						}
+					});
+				};
+				reader.readAsText(file);
+			});
+		});
+	});
+});
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-document.onreadystatechange = function () {
-    if (document.readyState === 'complete') {
-        console.log("this is a youtube page, so the remover works");
-        //console.log(Date.now());
-        chrome.runtime.onMessage.addListener(gotMessage);
-        //console.log("listener added on");
-        //console.log(Date.now());
+console.log("this is a youtube page, so the remover works");
+//console.log(Date.now());
+chrome.runtime.onMessage.addListener(gotMessage);
+//console.log("listener added on");
+//console.log(Date.now());
 
-        function gotMessage(message, sender, sendResponse) {
-            console.log("message received");
-            //console.log(message, sender, sendResponse);
-            sendResponse({txt:"response from the content script"});
-            //console.log(message.state);
-            if(message.state == "1") {
-                console.log("message state is: " + message.state);
-                setTimeout(function() {
-                    console.log("target: " + document.getElementById("related"));
-                    document.getElementById("related").style.display = "none";
-                    console.log("related videos removed!");
-                }, 800);
-            }
-        }
+function gotMessage(message, sender, sendResponse) {
+    console.log("message received");
+    //console.log(message, sender, sendResponse);
+    //console.log(message.state);
+    sendResponse({txt:"response from the content script"});
+    if(message.state == "1") {
+        console.log("message state is: " + message.state);
+        document.getElementById("related").style.display = "none";
+        setTimeout(function() {
+            console.log("target: " + document.getElementById("related"));
+            document.getElementById("related").style.display = "none";
+            console.log("related videos removed!");
+            console.log(document.getElementById("related").style.display);
+        }, 800);
+        document.getElementById("related").style.display = "none";
+        console.log(document.getElementById("related").style.display);
     }
 }
